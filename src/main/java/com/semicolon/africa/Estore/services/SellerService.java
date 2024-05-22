b839e58d9054b342acba9e37a5593da84be10f47
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:38:18 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/SellerService.java
+++ src/main/java/com/semicolon/africa/Estore/services/SellerService.java
@@ -1,8 +1,19 @@
 package com.semicolon.africa.Estore.services;
 
-import com.semicolon.africa.Estore.dtos.request.RegisterSellerRequest;
+import com.semicolon.africa.Estore.data.models.Seller;
+import com.semicolon.africa.Estore.dtos.request.*;
+import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
 import com.semicolon.africa.Estore.dtos.response.RegisterSellerResponse;
 
 public interface SellerService {
     RegisterSellerResponse registerSeller(RegisterSellerRequest request);
+    String login(LoginRequest loginRequest);
+    String logout(LogoutRequest logoutRequest);
+    Seller  findSeller(String sellerEmail);
+
+    AddProductResponse addProductToStore(AddProductRequest addProduct);
+
+    String deleteProduct(DeleteProductRequest deleteProductRequest);
+
+    String changeProductPrice(ChangeProductPriceRequest request);
 }
