bca3a4356dbdb3182881d89bd22b210828f20ae8
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:36:43 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/ProductService.java
+++ src/main/java/com/semicolon/africa/Estore/services/ProductService.java
@@ -1,4 +1,21 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Product;
+import com.semicolon.africa.Estore.dtos.request.AddProductRequest;
+import com.semicolon.africa.Estore.dtos.request.ChangeProductPriceRequest;
+import com.semicolon.africa.Estore.dtos.request.DeleteProductRequest;
+import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
+
+import java.math.BigDecimal;
+
 public interface ProductService {
+    long count();
+
+    AddProductResponse addProduct(AddProductRequest addProduct);
+
+    String deleteProduct(DeleteProductRequest deleteProductRequest);
+
+    Product findProductBy(long productId);
+
+    String changeProductPrice(ChangeProductPriceRequest request);
 }
