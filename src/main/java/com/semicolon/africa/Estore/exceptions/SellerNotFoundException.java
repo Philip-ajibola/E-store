039cd9d5c1f954ad914f3917f907aa0f7aadef73
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:37:48 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/SellerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class SellerNotFoundException extends EstoreExceptions {
+    public SellerNotFoundException(String message) {
+        super(message);
+    }
+}
