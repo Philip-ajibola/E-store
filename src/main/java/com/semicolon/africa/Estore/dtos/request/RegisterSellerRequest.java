cb8711552224c7ed3d416577edd1e5740cf5737f
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:36:56 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/dtos/request/RegisterSellerRequest.java
+++ src/main/java/com/semicolon/africa/Estore/dtos/request/RegisterSellerRequest.java
@@ -1,11 +1,16 @@
 package com.semicolon.africa.Estore.dtos.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterSellerRequest {
+    @NotNull(message = "name can't be null")
     private String seller_name;
+    @NotNull(message = "email can't be null")
     private String seller_email;
+    @NotNull(message = "phone number can't be null ")
     private String seller_phone;
+    @NotNull(message = "provide a password ")
     private String password;
 }
