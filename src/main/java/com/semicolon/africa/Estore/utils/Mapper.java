5bf9181dc9dfead8c7420d6d0dcdb71589ec1ba3
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:35:44 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/utils/Mapper.java
+++ src/main/java/com/semicolon/africa/Estore/utils/Mapper.java
@@ -1,8 +1,13 @@
 package com.semicolon.africa.Estore.utils;
 
+import com.semicolon.africa.Estore.data.models.Product;
 import com.semicolon.africa.Estore.data.models.Seller;
+import com.semicolon.africa.Estore.dtos.request.AddProductRequest;
 import com.semicolon.africa.Estore.dtos.request.RegisterSellerRequest;
+import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
 import com.semicolon.africa.Estore.dtos.response.RegisterSellerResponse;
+
+import java.math.RoundingMode;
 
 public class Mapper {
     public  static RegisterSellerResponse map(Seller seller) {
@@ -19,5 +24,20 @@ public class Mapper {
         seller.setPhone(request.getSeller_phone());
         seller.setPassword(request.getPassword());
         return seller;
+    }
+    public static Product map(AddProductRequest addProduct) {
+        Product product = new Product();
+        product.setProductDescription(addProduct.getProductDescription());
+        product.setProductName(addProduct.getProductName());
+        product.setProductPrice(addProduct.getProductPrice().setScale(2, RoundingMode.HALF_EVEN));
+        product.setCategory(addProduct.getCategory());
+        return product;
+    }
+    public static AddProductResponse map(Product product) {
+        AddProductResponse addProductResponse = new AddProductResponse();
+        addProductResponse.setProductDescription(product.getProductDescription());
+        addProductResponse.setProductName(product.getProductName());
+        addProductResponse.setProductId(product.getId());
+        return addProductResponse;
     }
 }
