1ef8073b1d81d6da6c374fe8eca1778f0fe62f37
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:34:13 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/EstoreExceptions.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class EstoreExceptions extends RuntimeException{
+    public EstoreExceptions(String message){
+        super(message);
+    }
+}
