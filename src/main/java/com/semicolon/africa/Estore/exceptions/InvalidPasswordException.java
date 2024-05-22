16e7d05bf81120c6ebf14488fc0b4ef14f828321
philip <ajibolaphilip10@gmail.com>
2024-05-22 22:34:39 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class InvalidPasswordException extends EstoreExceptions {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
