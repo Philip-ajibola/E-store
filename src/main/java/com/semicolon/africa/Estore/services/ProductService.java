3291a4dd32fd5b944ab0b9fe76d4d000e5f56d17
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:05:08 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/ProductService.java
+++ src/main/java/com/semicolon/africa/Estore/services/ProductService.java
@@ -1,12 +1,11 @@
 package com.semicolon.africa.Estore.services;
 
 import com.semicolon.africa.Estore.data.models.Product;
-import com.semicolon.africa.Estore.dtos.request.AddProductRequest;
-import com.semicolon.africa.Estore.dtos.request.ChangeProductPriceRequest;
-import com.semicolon.africa.Estore.dtos.request.DeleteProductRequest;
+import com.semicolon.africa.Estore.data.models.ProductCategory;
+import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 public interface ProductService {
     long count();
@@ -17,5 +16,13 @@ public interface ProductService {
 
     Product findProductBy(long productId);
 
-    String changeProductPrice(ChangeProductPriceRequest request);
+    String editProduct(EditProductRequest editProductReQuest);
+
+    void deleteAll();
+
+    AddProductResponse findProductByName(SearchForProductByNameRequest searchRequest);
+
+    List<AddProductResponse> findProductByCategory(ProductCategory productCategory);
+
+    List<Product> findAllProductBy(long sellerId);
 }
