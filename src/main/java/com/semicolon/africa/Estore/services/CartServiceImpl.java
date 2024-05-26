c9d6c5f9558acb596a0c509622758387c5a5d255
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:00:16 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CartServiceImpl.java
+++ src/main/java/com/semicolon/africa/Estore/services/CartServiceImpl.java
@@ -1,7 +1,38 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Item;
+import com.semicolon.africa.Estore.data.repositories.Carts;
+import com.semicolon.africa.Estore.dtos.request.AddItemToCartRequest;
+import com.semicolon.africa.Estore.dtos.request.RemoveItemFromCartRequest;
+import com.semicolon.africa.Estore.dtos.response.AddItemResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartServiceImpl implements CartServices{
+    @Autowired
+    private Carts carts;
+    @Autowired
+    private ItemService itemService;
+    @Override
+    public AddItemResponse addItemToCart(AddItemToCartRequest addItemToCartRequest) {
+        return itemService.addItem(addItemToCartRequest);
+    }
+
+    @Override
+    public long countCustomerItem(long customerId) {
+        return itemService.countCustomerItem(customerId);
+    }
+
+    @Override
+    public void removeItemFromCart(RemoveItemFromCartRequest removeItemFromCartRequest) {
+        itemService.deleteItem(removeItemFromCartRequest);
+    }
+
+    @Override
+    public List<Item> viewCart(Long id) {
+        return itemService.getItems(id);
+    }
 }
