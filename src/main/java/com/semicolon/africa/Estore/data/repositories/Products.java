546198db0b910b04781a0164e3438391ed9df8f6
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:04:57 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/data/repositories/Products.java
+++ src/main/java/com/semicolon/africa/Estore/data/repositories/Products.java
@@ -1,8 +1,17 @@
 package com.semicolon.africa.Estore.data.repositories;
 
 import com.semicolon.africa.Estore.data.models.Product;
+import com.semicolon.africa.Estore.data.models.ProductCategory;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface Products extends JpaRepository<Product, Long> {
+    Product findProductByProductName(@NotNull(message = "ProductName can't be null") String productName);
+
+    List<Product> findProductByCategory(ProductCategory productCategory);
+
+    List<Product> findAllProductBySellerId(long sellerId);
 }
