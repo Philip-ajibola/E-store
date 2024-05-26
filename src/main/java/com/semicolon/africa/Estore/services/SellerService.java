c08c54ca64360918b63f65139e11a7491da78063
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:07:16 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/SellerService.java
+++ src/main/java/com/semicolon/africa/Estore/services/SellerService.java
@@ -1,9 +1,13 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Product;
 import com.semicolon.africa.Estore.data.models.Seller;
 import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
 import com.semicolon.africa.Estore.dtos.response.RegisterSellerResponse;
+import org.springframework.beans.MutablePropertyValues;
+
+import java.util.List;
 
 public interface SellerService {
     RegisterSellerResponse registerSeller(RegisterSellerRequest request);
@@ -15,5 +19,11 @@ public interface SellerService {
 
     String deleteProduct(DeleteProductRequest deleteProductRequest);
 
-    String changeProductPrice(ChangeProductPriceRequest request);
+     String editProduct(EditProductRequest editProductReQuest);
+
+    long count();
+
+    String deactivateAccount(Long id);
+
+    List<Product> findAllProductAdded(long sellerId);
 }
