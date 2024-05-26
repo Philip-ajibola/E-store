9356c94be28f2539cb07dfad7c635ecfa3eaa357
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:08:00 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class UserNotFoundException extends EstoreExceptions {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
