1e603f69f731760911b57891710c1195c705e176
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:01:27 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CustomerService.java
+++ src/main/java/com/semicolon/africa/Estore/services/CustomerService.java
@@ -1,4 +1,25 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Customer;
+import com.semicolon.africa.Estore.dtos.request.*;
+import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
+import com.semicolon.africa.Estore.dtos.response.RegisterCustomerResponse;
+
+import java.util.List;
+
 public interface CustomerService {
+
+    long count();
+
+    RegisterCustomerResponse registerCustomer(RegisterCustomerRequest request);
+
+    String login(LoginRequest loginRequest);
+
+    Customer findCustomer(String email);
+
+    String logout(LogoutRequest loginRequest);
+
+    AddProductResponse searchForProductByName(SearchForProductByNameRequest searchRequest);
+
+    List<AddProductResponse> searchForProductByCategory(SearchForProductByCategoryRequest searchRequest);
 }
