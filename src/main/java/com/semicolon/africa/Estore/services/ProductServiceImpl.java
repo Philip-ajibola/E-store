242ddd1a1a1b7bc9d6ed59cdde7e61a4d9ea24c9
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:05:20 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/ProductServiceImpl.java
+++ src/main/java/com/semicolon/africa/Estore/services/ProductServiceImpl.java
@@ -1,18 +1,20 @@
 package com.semicolon.africa.Estore.services;
 
 import com.semicolon.africa.Estore.data.models.Product;
+import com.semicolon.africa.Estore.data.models.ProductCategory;
 import com.semicolon.africa.Estore.data.repositories.Products;
-import com.semicolon.africa.Estore.data.repositories.Sellers;
-import com.semicolon.africa.Estore.dtos.request.AddProductRequest;
-import com.semicolon.africa.Estore.dtos.request.ChangeProductPriceRequest;
-import com.semicolon.africa.Estore.dtos.request.DeleteProductRequest;
+import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
 import com.semicolon.africa.Estore.exceptions.InvalidProductNameException;
 import com.semicolon.africa.Estore.exceptions.ProductNotFoundException;
+import com.semicolon.africa.Estore.utils.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.semicolon.africa.Estore.utils.Mapper.map;
 
@@ -46,14 +48,43 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public String changeProductPrice(ChangeProductPriceRequest request) {
-        Product product = findProductBy(request.getProductId());
-        product.setProductPrice(request.getNewPrice());
+    public String editProduct(EditProductRequest editProductReQuest) {
+        Product product = findProductById(editProductReQuest.getProductId());
+        if(!Objects.equals(editProductReQuest.getProductPrice(), BigDecimal.valueOf(0))) product.setProductPrice(editProductReQuest.getProductPrice());
+        if(!editProductReQuest.getProductDescription().isBlank()) product.setProductDescription(editProductReQuest.getProductDescription());
+        if(editProductReQuest.getCategory() != ProductCategory.NONE) product.setCategory(editProductReQuest.getCategory());
+        if(!editProductReQuest.getProductName().isBlank()) product.setProductName(editProductReQuest.getProductName());
         products.save(product);
-        return "Price Changed SuccessFully";
+        return "Product Edited Successfully";
     }
 
-    private Product findProductById(long productId) {
+    @Override
+    public void deleteAll() {
+        products.deleteAll();
+    }
+
+    @Override
+    public AddProductResponse findProductByName(SearchForProductByNameRequest searchRequest) {
+        Product product = products.findProductByProductName(searchRequest.getProductName());
+        if(product == null) throw new ProductNotFoundException("Product Not Found");
+        return map(product);
+    }
+
+    @Override
+    public List<AddProductResponse> findProductByCategory(ProductCategory productCategory) {
+        List<Product> productList = products.findProductByCategory(productCategory);
+        return productList.stream().map(Mapper::map).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Product> findAllProductBy(long sellerId) {
+        List<Product> productList = products.findAllProductBySellerId(sellerId);
+        if(productList.isEmpty())throw new ProductNotFoundException("No Product Found");
+        return productList;
+    }
+
+
+    public Product findProductById(long productId) {
         return products.findById(productId).orElseThrow(()-> new ProductNotFoundException("Product not Found"));
     }
 
