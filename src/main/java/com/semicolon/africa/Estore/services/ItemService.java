bf06f29b9966d22a07db518cf22c1dfd5ecd6324
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:03:21 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/ItemService.java
+++ src/main/java/com/semicolon/africa/Estore/services/ItemService.java
@@ -1,4 +1,22 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Item;
+import com.semicolon.africa.Estore.dtos.request.AddItemToCartRequest;
+import com.semicolon.africa.Estore.dtos.request.RemoveItemFromCartRequest;
+import com.semicolon.africa.Estore.dtos.response.AddItemResponse;
+
+import java.util.List;
+
 public interface ItemService {
+    AddItemResponse addItem(AddItemToCartRequest addItemToCartRequest);
+
+     long count();
+
+    Item findItem(long itemId);
+
+    long countCustomerItem(long customerId);
+
+    void deleteItem(RemoveItemFromCartRequest removeItemFromCartRequest);
+
+    List<Item> getItems(long customerId);
 }
