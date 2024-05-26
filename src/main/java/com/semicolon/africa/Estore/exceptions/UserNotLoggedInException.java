09599c94e8500c591adc8a1573287695b3ad4947
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:08:10 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/UserNotLoggedInException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class UserNotLoggedInException extends EstoreExceptions {
+    public UserNotLoggedInException(String message) {
+        super(message);
+    }
+}
