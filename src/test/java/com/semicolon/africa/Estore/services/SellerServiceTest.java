92d77db3a2b2ac4a533b494f582ad9672f8ae315
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:07:47 +0100

    Initial commit

--- src/test/java/com/semicolon/africa/Estore/services/SellerServiceTest.java
+++ src/test/java/com/semicolon/africa/Estore/services/SellerServiceTest.java
@@ -6,9 +6,8 @@ import com.semicolon.africa.Estore.data.models.Seller;
 import com.semicolon.africa.Estore.data.repositories.Sellers;
 import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
-import com.semicolon.africa.Estore.exceptions.InvalidEmailException;
 import com.semicolon.africa.Estore.exceptions.InvalidPasswordException;
-import com.semicolon.africa.Estore.exceptions.InvalidUserNameException;
+import com.semicolon.africa.Estore.exceptions.ProductNotFoundException;
 import com.semicolon.africa.Estore.exceptions.SellerNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +18,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class SellerServiceTest {
@@ -33,27 +33,32 @@ public class SellerServiceTest {
     LoginRequest loginRequest;
     @BeforeEach
     public void setUp() {
+        registerSellerRequest = new RegisterSellerRequest();
+        registerSellerRequest.setSeller_name("Sellername");
+        registerSellerRequest.setSeller_email("ajibolaphilip10@gmail.com");
+        registerSellerRequest.setSeller_phone("SellerPhone");
+        registerSellerRequest.setPassword("Seller_password");
+
         addProduct = new AddProductRequest();
         addProduct.setProductName("productName");
         addProduct.setProductDescription("Product_description");
         addProduct.setProductPrice(BigDecimal.valueOf(2000));
         addProduct.setCategory(ProductCategory.CLOTHING);
 
-         registerSellerRequest = new RegisterSellerRequest();
-        registerSellerRequest.setSeller_name("Sellername");
-        registerSellerRequest.setSeller_email("ajibolaphilip10@gmail.com");
-        registerSellerRequest.setSeller_phone("SellerPhone");
-        registerSellerRequest.setPassword("Seller_password");
 
         loginRequest = new LoginRequest();
         loginRequest.setEmail(registerSellerRequest.getSeller_email());
         loginRequest.setPassword(registerSellerRequest.getPassword());
+
+        sellers.deleteAll();
+        productService.deleteAll();
     }
     @Test
     public void testThatSellerCanRegister(){
 
         sellerService.registerSeller(registerSellerRequest);
         assertEquals("Sellername",sellers.findByEmail(registerSellerRequest.getSeller_email()).getUsername());
+        assertEquals(1,sellerService.count());
     }
     @Test
     public void testThatSellerCanFindSeller(){
@@ -87,39 +92,67 @@ public class SellerServiceTest {
     }
     @Test
     public void testThatUserCanAddProductToTheStore(){
-        sellerService.registerSeller(registerSellerRequest);
+        long id = sellerService.registerSeller(registerSellerRequest).getId();
         sellerService.login(loginRequest);
+        addProduct.setSellerId(id);
         sellerService.addProductToStore(addProduct);
         assertEquals(1,productService.count());
     }
     @Test
     public void testThatSellerCanDeleteProductFromTheStore(){
-        sellerService.registerSeller(registerSellerRequest);
+        long id = sellerService.registerSeller(registerSellerRequest).getId();
         sellerService.login(loginRequest);
+        addProduct.setSellerId(id);
         AddProductResponse addProductResponse = sellerService.addProductToStore(addProduct);
         assertEquals(1,productService.count());
 
         DeleteProductRequest deleteProductRequest = new DeleteProductRequest();
         deleteProductRequest.setProductId(addProductResponse.getProductId());
+        deleteProductRequest.setSellerId(id);
         sellerService.deleteProduct(deleteProductRequest);
         assertEquals(0,productService.count());
     }
     @Test
-    public void testThatProductPriceCanBeEdited(){
+    public void testThatProductNameCanBeChanged(){
         sellerService.registerSeller(registerSellerRequest);
         sellerService.login(loginRequest);
         AddProductResponse addProductResponse = sellerService.addProductToStore(addProduct);
-        ChangeProductPriceRequest request = new ChangeProductPriceRequest();
-        request.setProductId(addProductResponse.getProductId());
-        request.setNewPrice(BigDecimal.valueOf(3000));
-        sellerService.changeProductPrice(request);
-
+        EditProductRequest editProductReQuest = new EditProductRequest();
+        editProductReQuest.setProductName("newName");
+        editProductReQuest.setProductId(addProductResponse.getProductId());
+        sellerService.editProduct(editProductReQuest);
         Product product  = productService.findProductBy(addProductResponse.getProductId());
-        assertEquals(BigDecimal.valueOf(3000).setScale(2, RoundingMode.HALF_EVEN),product.getProductPrice());
+        assertEquals("newName",product.getProductName());
+        assertEquals(BigDecimal.valueOf(2000).setScale(2,RoundingMode.HALF_EVEN),product.getProductPrice());
     }
     @Test
-    public void testThatProductNameCanBeChanged(){
-
+    public  void testThatProductWithInvalidIdCannotBeEdited(){
+        sellerService.registerSeller(registerSellerRequest);
+        sellerService.login(loginRequest);
+        AddProductResponse addProductResponse = sellerService.addProductToStore(addProduct);
+        EditProductRequest editProductReQuest = new EditProductRequest();
+        editProductReQuest.setProductName("newName");
+        editProductReQuest.setProductId(addProductResponse.getProductId()+3);
+        Product product = productService.findProductBy(addProductResponse.getProductId());
+        assertThrows(ProductNotFoundException.class,()->sellerService.editProduct(editProductReQuest));
+        assertThat(product.getProductName(), equalTo(addProductResponse.getProductName()));
     }
+    @Test
+    public void testThatSellerCanDeActivateAccount(){
+        sellerService.registerSeller(registerSellerRequest);
+        sellerService.login(loginRequest);
+        Seller seller = sellerService.findSeller(registerSellerRequest.getSeller_email());
+        sellerService.deactivateAccount(seller.getId());
+        assertThat(sellerService.count(),equalTo(0L));
+    }
+    @Test
+    public void testThatUserCanGetAllProductAdded(){
+        long sellerId =sellerService.registerSeller(registerSellerRequest).getId();
+        sellerService.login(loginRequest);
+        addProduct.setSellerId(sellerId);
+        sellerService.addProductToStore(addProduct);
+        assertThat(sellerService.findAllProductAdded(sellerId).size(), equalTo(1));
+    }
+
 
 }
