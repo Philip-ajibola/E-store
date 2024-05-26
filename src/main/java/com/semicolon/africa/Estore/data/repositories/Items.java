53d16fc9dcc94256cca0fb8f0dca1ea3e9c703c8
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:03:04 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/data/repositories/Items.java
+++ src/main/java/com/semicolon/africa/Estore/data/repositories/Items.java
@@ -3,5 +3,8 @@ package com.semicolon.africa.Estore.data.repositories;
 import com.semicolon.africa.Estore.data.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface Items extends JpaRepository<Item, Long> {
+    List<Item> findItemByCustomerId(long customerId);
 }
