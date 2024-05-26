74b0d2ab1f3838c0e80ee8fea120e2930ebf33de
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:00:29 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CartServices.java
+++ src/main/java/com/semicolon/africa/Estore/services/CartServices.java
@@ -1,4 +1,20 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Item;
+import com.semicolon.africa.Estore.dtos.request.AddItemToCartRequest;
+import com.semicolon.africa.Estore.dtos.request.RemoveItemFromCartRequest;
+import com.semicolon.africa.Estore.dtos.response.AddItemResponse;
+import org.springframework.beans.MutablePropertyValues;
+
+import java.util.List;
+
 public interface CartServices {
+    AddItemResponse addItemToCart(AddItemToCartRequest addItemToCartRequest);
+
+    long countCustomerItem(long customerId);
+
+    void removeItemFromCart(RemoveItemFromCartRequest removeItemFromCartRequest);
+
+    List<Item> viewCart(Long id);
 }
+
