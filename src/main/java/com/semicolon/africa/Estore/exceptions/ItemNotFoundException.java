01357935b58352c4ca48b46d2c96cd90b38099c4
philip <ajibolaphilip10@gmail.com>
2024-05-26 01:02:34 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class ItemNotFoundException extends EstoreExceptions {
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
