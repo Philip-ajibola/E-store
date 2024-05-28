0f898c39d54fc994aaec4c2e6fe266ffe473709b
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:21:46 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/BillingFormationService.java
+++ src/main/java/com/semicolon/africa/Estore/services/BillingFormationService.java
@@ -1,4 +1,7 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.BillingInformation;
+
 public interface BillingFormationService {
+    BillingInformation save(BillingInformation billingInformation);
 }
