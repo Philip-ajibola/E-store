3d8e2c04ae9c8ecf09f44a62ea579546f8aa3b12
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:24:09 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CustomerService.java
+++ src/main/java/com/semicolon/africa/Estore/services/CustomerService.java
@@ -1,8 +1,10 @@
 package com.semicolon.africa.Estore.services;
 
 import com.semicolon.africa.Estore.data.models.Customer;
+import com.semicolon.africa.Estore.data.models.Order;
 import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
+import com.semicolon.africa.Estore.dtos.response.PlaceOrderResponse;
 import com.semicolon.africa.Estore.dtos.response.RegisterCustomerResponse;
 
 import java.util.List;
@@ -22,4 +24,9 @@ public interface CustomerService {
     AddProductResponse searchForProductByName(SearchForProductByNameRequest searchRequest);
 
     List<AddProductResponse> searchForProductByCategory(SearchForProductByCategoryRequest searchRequest);
+
+    PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest,CreateAddressRequest createAddressRequest,CreateBillingFormatRequest createBillingFormatRequest);
+
+    void receiverOrder(Order order, long customerId);
 }
+
