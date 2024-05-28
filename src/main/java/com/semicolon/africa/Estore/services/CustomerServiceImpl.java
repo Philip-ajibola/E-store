0a930867274590f876b4dc6184c077ce5f72c1a2
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:24:23 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CustomerServiceImpl.java
+++ src/main/java/com/semicolon/africa/Estore/services/CustomerServiceImpl.java
@@ -1,13 +1,15 @@
 package com.semicolon.africa.Estore.services;
 
-import com.semicolon.africa.Estore.data.models.Customer;
+import com.semicolon.africa.Estore.data.models.*;
 import com.semicolon.africa.Estore.data.repositories.Customers;
 import com.semicolon.africa.Estore.dtos.request.*;
 import com.semicolon.africa.Estore.dtos.response.AddProductResponse;
+import com.semicolon.africa.Estore.dtos.response.PlaceOrderResponse;
 import com.semicolon.africa.Estore.dtos.response.RegisterCustomerResponse;
 import com.semicolon.africa.Estore.exceptions.InvalidPasswordException;
 import com.semicolon.africa.Estore.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +20,9 @@ import static com.semicolon.africa.Estore.utils.Mapper.map;
 public class CustomerServiceImpl implements CustomerService{
     @Autowired
     private Customers customers;
+    @Autowired
+    @Lazy
+    private OrderService orderService;
     @Autowired
     private ProductService productService;
     @Override
@@ -62,6 +67,19 @@ public class CustomerServiceImpl implements CustomerService{
     @Override
     public List<AddProductResponse> searchForProductByCategory(SearchForProductByCategoryRequest searchRequest) {
         return productService.findProductByCategory(searchRequest.getProductCategory());
+    }
+
+    @Override
+    public PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest, CreateAddressRequest createAddressRequest, CreateBillingFormatRequest createBillingFormatRequest) {
+       return orderService.placeOrder(placeOrderRequest, createAddressRequest, createBillingFormatRequest);
+
+    }
+
+    @Override
+    public void receiverOrder(Order order, long customerId) {
+        Customer customer = customers.findById(customerId).orElseThrow(()->new UserNotFoundException("User Not Found"));
+        customer.getListOfOrders().add(order);
+        customers.save(customer);
     }
 
 
