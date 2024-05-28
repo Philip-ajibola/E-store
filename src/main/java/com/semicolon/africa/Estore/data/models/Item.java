55403918b7fd8a4f153c0e890a0df3e4dbde259e
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:24:56 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/data/models/Item.java
+++ src/main/java/com/semicolon/africa/Estore/data/models/Item.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @Entity
 @Table(name="items")
 public class Item {
     private Integer quantity;
+    private BigDecimal productPrice = new BigDecimal("0.0");
     @JoinColumn
     private Long productId;
     @JoinColumn
