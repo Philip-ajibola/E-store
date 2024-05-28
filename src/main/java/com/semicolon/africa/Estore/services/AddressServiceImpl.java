fe531bbcd8f06270797d8b3fcd12dead9ff08dbb
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:21:32 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/AddressServiceImpl.java
+++ src/main/java/com/semicolon/africa/Estore/services/AddressServiceImpl.java
@@ -1,7 +1,16 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.Address;
+import com.semicolon.africa.Estore.data.repositories.Addresses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl implements AddressesServices{
+    @Autowired
+    private Addresses addresses;
+    @Override
+    public Address save(Address address) {
+       return addresses.save(address);
+    }
 }
