f06c7c416072a515c4ebe91494c5414909b1bc19
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:22:01 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/BillingFormatServiceImpl.java
+++ src/main/java/com/semicolon/africa/Estore/services/BillingFormatServiceImpl.java
@@ -1,7 +1,16 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.models.BillingInformation;
+import com.semicolon.africa.Estore.data.repositories.BillingInformations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BillingFormatServiceImpl implements BillingFormationService{
+    @Autowired
+    private BillingInformations billingInformations;
+    @Override
+    public BillingInformation save(BillingInformation billingInformation) {
+        return billingInformations.save(billingInformation);
+    }
 }
