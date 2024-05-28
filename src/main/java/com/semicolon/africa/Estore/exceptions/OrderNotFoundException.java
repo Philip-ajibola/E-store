1fd2a6fc959a314af3d0494f4e16a7898a7f46ac
philip <ajibolaphilip10@gmail.com>
2024-05-28 09:25:42 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class OrderNotFoundException extends EstoreExceptions {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
