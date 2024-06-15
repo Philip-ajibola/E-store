72bba4590a6831a9a5a8d55ded0f7f41fa524bd4
philip <ajibolaphilip10@gmail.com>
2024-06-16 00:10:16 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/dtos/response/AddItemResponse.java
+++ src/main/java/com/semicolon/africa/Estore/dtos/response/AddItemResponse.java
@@ -6,9 +6,6 @@ import java.math.BigDecimal;
 
 @Data
 public class AddItemResponse {
+    private Long cartId;
     private Long itemId;
-    private String productName;
-    private BigDecimal price;
-    private int quantity;
-
 }
