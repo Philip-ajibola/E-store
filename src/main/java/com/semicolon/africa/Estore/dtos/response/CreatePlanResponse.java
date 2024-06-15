f3b73856191a28089a9b69835f5e7c2a3d389382
philip <ajibolaphilip10@gmail.com>
2024-06-16 00:13:21 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/dtos/response/CreatePlanResponse.java
+++ src/main/java/com/semicolon/africa/Estore/dtos/response/CreatePlanResponse.java
@@ -1,4 +1,47 @@
 package com.semicolon.africa.Estore.dtos.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CreatePlanResponse {
+
+    private Boolean status;
+    private String message;
+    private Data data;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public class Data {
+
+        private String name;
+
+        private String amount;
+
+        private String interval;
+
+        private String integration;
+
+        private String planCode;
+
+        private String sendInvoices;
+
+        private String sendSms;
+
+        private String currency;
+
+       private String id;
+
+        private String createdAt;
+
+        private String updatedAt;
+
+    }
 }
