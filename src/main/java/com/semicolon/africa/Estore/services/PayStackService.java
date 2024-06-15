99e8ad380acea8dec990647ef4ea7263e2f6807b
philip <ajibolaphilip10@gmail.com>
2024-06-16 00:23:27 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/PayStackService.java
+++ src/main/java/com/semicolon/africa/Estore/services/PayStackService.java
@@ -1,4 +1,14 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.dtos.request.CreatePlanDto;
+import com.semicolon.africa.Estore.dtos.request.InitializePaymentDto;
+import com.semicolon.africa.Estore.dtos.response.CreatePlanResponse;
+import com.semicolon.africa.Estore.dtos.response.InitializePaymentResponse;
+import com.semicolon.africa.Estore.dtos.response.PaymentVerificationResponse;
+
 public interface PayStackService {
+    CreatePlanResponse createPlan(CreatePlanDto createPlanDto) throws Exception;
+    InitializePaymentResponse initializePayment(InitializePaymentDto initializePaymentDto);
+    PaymentVerificationResponse paymentVerification(String reference, String plan, Long id) throws Exception;
+
 }
