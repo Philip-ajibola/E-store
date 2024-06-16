ec6f2dbc0c465d0c03f26aa6ccf9451e7c51e1bf
philip <ajibolaphilip10@gmail.com>
2024-06-16 01:24:15 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/constant/APIConstants.java
+++ src/main/java/com/semicolon/africa/Estore/constant/APIConstants.java
@@ -1,9 +1,14 @@
 package com.semicolon.africa.Estore.constant;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class APIConstants {
-    private static final int STATUS_CODE_OK = 200;
-    private static final int STATUS_CODE_CREATED = 201;
-    private static final String PAYSTACK_INIT = "https://api.paystack.co/transaction/initialize";
-    private static final String PAYSTACK_VERIFY = "https://api.paystack.co/transaction/verify/";
-    private static final String PAYSTACK_INITIALIZE_PAY = "https://api.paystack.co/transaction/initialize";
+    public static final int STATUS_CODE_OK = 200;
+    public static final int STATUS_CODE_CREATED = 201;
+    public static final String PAYSTACK_INIT = "https://api.paystack.co/transaction/initialize";
+    public static final String PAYSTACK_VERIFY = "https://api.paystack.co/transaction/verify/";
+    public static final String PAYSTACK_INITIALIZE_PAY = "https://api.paystack.co/transaction/initialize";
 }
