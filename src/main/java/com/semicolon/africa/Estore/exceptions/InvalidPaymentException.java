b891597beb0b2cbafb5e6ee3a90b635f643a85a3
philip <ajibolaphilip10@gmail.com>
2024-06-27 01:35:57 +0100

    Initial commit

---
+++ src/main/java/com/semicolon/africa/Estore/exceptions/InvalidPaymentException.java
@@ -0,0 +1,7 @@
+package com.semicolon.africa.Estore.exceptions;
+
+public class InvalidPaymentException extends EstoreExceptions{
+    public InvalidPaymentException(String message) {
+        super(message);
+    }
+}
