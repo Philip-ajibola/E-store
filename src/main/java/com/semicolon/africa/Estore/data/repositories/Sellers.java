f01bed36f54ebf890e9cf95a2d4522092afad131
philip <ajibolaphilip10@gmail.com>
2024-07-13 22:18:36 +0100

    so many changes were made

--- src/main/java/com/semicolon/africa/Estore/data/repositories/Sellers.java
+++ src/main/java/com/semicolon/africa/Estore/data/repositories/Sellers.java
@@ -1,9 +1,3 @@
 package com.semicolon.africa.Estore.data.repositories;
 
-import com.semicolon.africa.Estore.data.models.Seller;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Sellers extends JpaRepository<Seller, Long> {
- Seller findByEmail(String email);
- boolean existsByEmail(String email);
-}
