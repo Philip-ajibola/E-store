5802f944e973d95b22a3ca07c0918b84fd8c39d4
philip <ajibolaphilip10@gmail.com>
2024-07-13 22:16:53 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/JwtService.java
+++ src/main/java/com/semicolon/africa/Estore/services/JwtService.java
@@ -5,16 +5,20 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
 public class JwtService {
     private  String  SECRET_KEY = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
     private final long EXPIRATION_TIME = 864_000_000; // 10 days
+    private final long refreshExpiration = 604800000;
 
     public String extractUsername(String username){
         return extractClaims(username, Claims::getSubject);
@@ -38,22 +42,28 @@ public class JwtService {
         return extractClaims(token, Claims::getExpiration);
     }
     public String generateToken(UserDetails userDetails){
-        return generateToken(new HashMap<>(),userDetails);
+        return generateToken(new HashMap<>(),userDetails,EXPIRATION_TIME);
     }
 
-    private String generateToken(Map<String,Object> claims,UserDetails userDetails){
+    private String generateToken(Map<String,Object> claims,UserDetails userDetails,Long EXPIRATION_TIME){
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        List<String> roles = authorities.stream()
+                .map((grantedAuthority)->grantedAuthority.getAuthority())
+                .collect(Collectors.toList());
         return Jwts.builder()
                .setClaims(claims)
                .setSubject(userDetails.getUsername())
+                .claim("role",roles)
                .setIssuedAt(new Date(System.currentTimeMillis()))
                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                .signWith(getKey(SECRET_KEY), SignatureAlgorithm.HS256)
                .compact();
     }
-    public String generateRefreshToken(UserDetails userDetails){
-        return generateToken(new HashMap<>(), userDetails);
-    }
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public String generateRefreshToken(
+            UserDetails userDetails
+    ) {
+        return generateToken(new HashMap<>(), userDetails, refreshExpiration);
+    }    public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
