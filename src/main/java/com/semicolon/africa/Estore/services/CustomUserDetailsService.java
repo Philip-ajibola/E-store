93362b4bf52c6f57f6e5026b6bc957126910d047
philip <ajibolaphilip10@gmail.com>
2024-07-13 22:15:28 +0100

    Initial commit

--- src/main/java/com/semicolon/africa/Estore/services/CustomUserDetailsService.java
+++ src/main/java/com/semicolon/africa/Estore/services/CustomUserDetailsService.java
@@ -1,7 +1,7 @@
 package com.semicolon.africa.Estore.services;
 
+import com.semicolon.africa.Estore.data.repositories.Admins;
 import com.semicolon.africa.Estore.data.repositories.Customers;
-import com.semicolon.africa.Estore.data.repositories.Sellers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final Sellers sellers;
+    private final Admins admins;
     private final Customers customers;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserDetails userDetails = sellers.findByEmail(username);
+        UserDetails userDetails = admins.findByEmail(username);
         if(userDetails == null) userDetails = customers.findByEmail(username);
         if(userDetails == null) throw new UsernameNotFoundException("User Not Found");
         return userDetails;
